ae63c75e73f344544c2fc7eda210488f6da45ef5
Patrick Pedersen <ctx.xda@gmail.com>
2018-03-22 12:33:25 +0100

    Examples: Added example
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

---
+++ examples/Example.ino
@@ -0,0 +1,47 @@
+/*
+
+ * Copyright (c) 2018 Patrick Pedersen <ctx.xda@gmail.com>
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
+ */
+
+#include <Arduino.h>
+
+/* Include this library */
+#include "Segment.h"
+#include "Digit.h"
+#include "Display.h"
+
+const byte digit_pins[4] {6, 9, 10, 0}; // From com1 - com4
+const byte segment_pins[7] {7, 11, 2, 4, 5, 8, 1}; // From a - g (abc...g)
+const byte dp_pin = 3;
+
+Display *d;
+
+void setup() {
+  for (int i = 0; i <= A5; i++) {
+    pinMode(i, OUTPUT);
+  }
+
+  d = new Display(digit_pins, segment_pins, dp_pin);
+}
+
+void loop() {
+  d->print("1.234"); // Must be looped!
+}
